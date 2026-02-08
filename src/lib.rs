acea4ae839482a72b845e06d30c5dd9176af08cd
LE55CUB3 <rainyhowcool@outlook.com>
2026-02-08 09:53:36 +0800

    Fix a serious bug caused application cannot to build

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #[derive(Debug)]
-#[allow(unused)]
 pub struct Operation {
     opcode: u8,
     opt1: i32,
@@ -16,7 +15,7 @@ impl Operation {
     }
 
     pub fn generate(&self) -> Vec<u8> {
-        if self.opcode <= 2 {
+        if self.opcode <= 1 {
             // TODO: 加紧修理这坨💩
             // NOTE: 给人类9178年都写不出这种代码
             return vec![0x32, self.opcode << 4 | self.opt1 as u8, 
@@ -24,6 +23,10 @@ impl Operation {
                     (self.opt2 >> 8 << 24 >> 24) as u8,
                     (self.opt2 >> 16 << 24 >> 24) as u8,
                     (self.opt2 >> 24) as u8];
-        } vec![]
+        } else if self.opcode <= 2 {
+            return vec![0xFF, self.opcode << 4 | self.opt1 as u8,
+                self.opt2 as u8]
+        } 
+        vec![]
     }
 }
--- src/vmassembler/main.rs
+++ src/vmassembler/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use lalrpop_util::lalrpop_mod;
-use std::fs::{read_to_string, File};
+use std::fs::{read_to_string, File, self};
 use std::io::Write;
 
 lalrpop_mod!(asm);
@@ -15,12 +15,17 @@ struct Args {
     // Output Path
     #[clap(short, long, default_value = "a.out")]
     out: String,
+
+    // Data segment raw file
+    #[clap(short, long)]
+    data_file: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Input file: {}", args.input);
+    println!("Input file: {}", &args.input);
+    println!("Data segment raw file: {}", &args.data_file);
 
     let input = read_to_string(args.input).unwrap();
 
@@ -36,11 +41,35 @@ fn main() {
                     0x00 // Compress Option (1 means lz)
     ];
 
+    let mut seg_table: [u8; 23] = [0x02, // Segment count
+            0x01, // Execuable
+            0x5, b'.', b'c', b'o', b'd', b'e', // segment length and name
+            31, 0x0, 0x0, 0x0, // Offest(header.len + seg_table.len)
+            0x00, // Unexecuable
+            0x5, b'.', b'd', b'a', b't', b'a', // segment length and name
+            0x0, 0x0, 0x0, 0x0 // Offest(unknown)
+    ];
+
     file.write(header).unwrap();
 
+    let mut generated_code_segment: Vec<u8> = vec![];
+
     for operation in ast {
-        file.write(&operation.generate()).unwrap();
+        generated_code_segment.extend(operation.generate());
     }
+
+    let len: i32 = 31 + generated_code_segment.len() as i32;
+
+    seg_table[19] = len as u8;
+    seg_table[20] = (len >> 8) as u8;
+    seg_table[21] = (len >> 16) as u8;
+    seg_table[22] = (len >> 24) as u8;
+
+    let data_segment: Vec<u8> = fs::read(args.data_file).unwrap();
+
+    file.write(&seg_table).unwrap();
+    file.write(&generated_code_segment).unwrap();
+    file.write(&data_segment).unwrap();
 
     println!("Outputed in {}", &args.out);
 }
